a81a79dc4381c92d79a5b4f9436d1417a85e704e
Nick <79202985834@mail.ru>
2021-11-16 15:59:38 +0300

    lesson_1

---
+++ vue/fp/src/components/HelloWorld.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">
+    {{ message }}
+    <br />
+    <input type="number" placeholder="operand1" v-model.number="operand1" />
+    <input type="number" placeholder="operand2" v-model.number="operand2" />
+    <br />
+    {{ result }}
+    <br />
+    <button v-on:click="result = operand1 + operand2">+</button>
+    <button v-on:click="result = operand1 - operand2">-</button>
+    <button @click="result = operand1 * operand2">*</button>
+    <button @click="result = operand1 / operand2">/</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      message: "HelloVue",
+      result: 0,
+      operand1: 0,
+      operand2: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.result = this.operand1 + this.operand2;
+    },
+    substract() {
+      this.result = this.operand1 - this.operand2;
+    },
+    divide() {
+      this.result = this.operand1 / this.operand2;
+    },
+    muliply() {
+      this.result = this.operand1 * this.operand2;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
